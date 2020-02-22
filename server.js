6b118c473ca44108b2ddf42f4a00c0a639e407c6
Nagma Bardoi <nagmabardoi@gmail.com>
2020-02-22 11:56:02 -0600

    making react work with heroku

--- server.js
+++ server.js
@@ -22,6 +22,15 @@ app.use((req, res, next) => {
   next();
 });
 
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
+
 app.use(bodyParser.json());
 
 app.use('/api', routes);
